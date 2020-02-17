906c2fbdbfeb2148db0649ed10d54e1e0ff9e83d
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-02-17 15:46:55 +0000

    Test for object rest operator

--- test-files/ts-pass.ts
+++ test-files/ts-pass.ts
@@ -2,4 +2,15 @@ function getMaNumberFunction(value: number, ...rest: Array<number>) {
   return value + rest.reduce((acc: number, curr: number) => acc + curr);
 }
 
+const bazz = {
+  baz: 'baz',
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const anObject = {
+  ...bazz,
+  foo: 'fo',
+  bar: 'bat',
+};
+
 getMaNumberFunction(3, 4, 2, 1);
