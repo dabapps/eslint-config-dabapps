634f66d738e865c485a91a47a7ce4fd8aed58e5e
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-03-25 11:35:54 +0000

    Update import/order rules

--- test-files/imports-fail.js
+++ test-files/imports-fail.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Dom from 'react-dom';
 
 import nooo from '../base/test';
-
 import { foo as fu } from './js-fail';
 import { baz as bart } from './imports-pass';
 
